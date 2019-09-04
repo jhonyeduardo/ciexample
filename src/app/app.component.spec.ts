2078e3601e08f9b36cc7ff0b0ff9e218f78e07c5
jhonyeduardo <jhonyeduardo96@gmail.com>
2019-09-03 23:44:16 -0300

    ajuste teste unitario

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ciexample!');
+    expect(compiled.querySelector('h1').textContent).toContain('Testando Preview Deploy Netlify!!');
   });
 });
